f7b0b5cc31d2ae2dfb7ef79306122773ff645817
Eder Lara <ederlatru@gmail.com>
2025-10-01 20:50:40 -0500

    Procedimientos almacenados

---
+++ Procedimientos Almacenados/Mysql Procedure.sql
@@ -0,0 +1,48 @@
+CREATE PROCEDURE `cost_item`()
+BEGIN
+
+    -- Declaración de variables locales
+    DECLARE fin_cursor INT;           -- Indicador para saber si el cursor ha llegado al final
+    DECLARE porcentaje FLOAT;         -- Porcentaje de utilidad calculado aleatoriamente
+    DECLARE venta FLOAT;              -- Precio de venta calculado
+    DECLARE id INT;                   -- ID del artículo
+    DECLARE valor FLOAT;              -- Precio base del artículo
+
+    -- Declaración del cursor 'item_set' para recorrer los artículos
+    DECLARE item_set CURSOR FOR 
+        SELECT
+            id_articulo,                                							-- ID del artículo
+            precio,                                     							-- Precio base
+            (10 + (RAND() * 11)) AS porcentaje_utilidad, 							-- Porcentaje aleatorio entre 10 y 21
+            ROUND(precio * (1 + FLOOR(10 + (RAND() * 11))/100), 2) AS precio_venta 	-- Precio de venta con utilidad
+        FROM articulos;
+
+    -- Manejador para detectar el fin del cursor
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET fin_cursor = 1;
+
+    -- Abrimos el cursor para comenzar a recorrer los registros
+    OPEN item_set;
+
+    -- Bucle para recorrer cada fila del cursor
+    bucle: LOOP
+        -- Extraemos los valores de la fila actual del cursor
+        FETCH item_set INTO id, valor, porcentaje, venta;
+
+        -- Mostramos los valores obtenidos (opcional, útil para depuración)
+        -- SELECT id, valor, porcentaje, venta;
+
+        -- Si se ha llegado al final del cursor, salimos del bucle
+        IF fin_cursor = 1 THEN 
+            LEAVE bucle;
+        END IF;
+
+        -- Actualizamos el artículo con el nuevo porcentaje de utilidad y precio de venta
+        UPDATE articulos
+            SET porcentaje_utilidad = porcentaje, 
+                precio_venta = venta
+            WHERE id_articulo = id;
+    END LOOP;
+
+    -- Cerramos el cursor después de terminar el recorrido
+    CLOSE item_set;
+END
--- app ingesta/config/config.py
+++ app ingesta/config/config.py
@@ -9,7 +9,7 @@ DB_CONFIG = {
     "mysql_source": "mysql+mysqlconnector://user:secret@localhost:3306/tienda_mx",
     "oracle": "oracle+oracledb://appuser:appuserpwd@localhost:1521/?service_name=XEPDB1",
     "postgres": "postgresql+psycopg2://user:secret@localhost:5432/almacen_pg",
-    "sqlserver": "mssql+pyodbc://sa:StrongP@ssword1@127.0.0.1:1433/ventas_sql?driver=ODBC+Driver+17+for+SQL+Server",
+    "sqlserver": "mssql+pyodbc://sa:StrongP%40ssword1@127.0.0.1:1433/ventas_sql?driver=ODBC+Driver+17+for+SQL+Server",
     "mariadb": "mariadb+mariadbconnector://user_maria:secret_maria@localhost:3307/tienda_maria", 
     
     # Destino de Carga 
